996f59a372cb656ab08fa7dd72721ba8a8163c95
LuanDevecchi <gitluan@engineer.com>
2020-10-02 06:21:32 -0300

    Create fizzbuzz.cc
    funny fizzbuzz solution :p

---
+++ fizzbuzz.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <map>
+
+std::map<bool, std::string> lookuptablefizz
+{
+	{true, "Fizz"},
+	{false, ""},
+};
+
+std::map<bool, std::string> lookuptablebuzz
+{
+	{true, "Buzz"},
+	{false, ""},
+};
+
+int main()
+{
+	for (int i{ 0 }; i < 100; ++i)
+	{
+		const bool fizz_flag = i % 3 == 0;
+		const bool buzz_flag = i % 5 == 0;
+		if ( fizz_flag || buzz_flag )
+		{
+			std::cout << lookuptablefizz[fizz_flag] << lookuptablebuzz[buzz_flag] << std::endl;
+		}
+		else
+		{
+			std::cout << i << std::endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
